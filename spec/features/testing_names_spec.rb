a0286aac8e56d5dc97b6636f30893753bf351d81
OrganicAC <anthonychaplin17@gmail.com>
2022-01-07 16:43:42 +0000

    added web helpers

--- spec/features/testing_names_spec.rb
+++ spec/features/testing_names_spec.rb
@@ -1,10 +1,6 @@
 feature 'Enter names' do
     scenario 'submitting names' do
-      visit('/')
-      fill_in :player_1_name, with: 'Jack'
-      fill_in :player_2_name, with: 'Jill'
-      click_button 'Submit'
-
+      sign_in_and_play
       expect(page).to have_content 'Jack vs. Jill'
     end
 end 
--- spec/features/viewing_hitpoints_spec.rb
+++ spec/features/viewing_hitpoints_spec.rb
@@ -1,10 +1,6 @@
 feature 'View hitpoints' do
     scenario 'Can view hitpoints of player 2' do
-    visit('/')
-    fill_in :player_1_name, with: 'Jack'
-    fill_in :player_2_name, with: 'Jill'
-    click_button 'Submit'
-
+    sign_in_and_play
     expect(page).to have_content 'Jill - 50 hit points'
     end 
 end
